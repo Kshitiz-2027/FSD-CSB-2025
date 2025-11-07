aa8df1684d603d3498e44745eaf51cb4dea35358
Kshitiz-2027 <kshitiz2004srps@gmail.com>
2025-11-07 10:04:18 +0530

    this is image manipulation

--- ReactWork/Reactviteapp/src/App.jsx
+++ ReactWork/Reactviteapp/src/App.jsx
@@ -3,16 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // import Gallery from './components/Gallery.jsx'
-import Statehandling from './components/Statehandling.jsx'
+// import Statehandling from './components/Statehandling.jsx'
+import ImageManipulation from './components/ImageManipulation.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
-      <h2>Welcome to react Vite</h2>
+      {/* <h2>Welcome to react Vite</h2> */}
       {/* <Gallery /> */}
-      <Statehandling />
+      {/* <Statehandling /> */}
+      <ImageManipulation />
     </div>
   )
 }
