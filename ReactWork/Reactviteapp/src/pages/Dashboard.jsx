8837535e92e616ee61ce7552046c1d389ba2c02f
Kshitiz-2027 <kshitiz2004srps@gmail.com>
2025-11-26 09:56:58 +0530

    added for reference

--- ReactWork/Reactviteapp/src/pages/Dashboard.jsx
+++ ReactWork/Reactviteapp/src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 
-const dashboard = () => {
+function dashboard() {
+  const[counter,setCounter]=useState(10);
+  const[pointer,setPointer]=useState(100);
+  useEffect(()=>{
+    console.log(counter)
+    console.log(pointer)
+},[counter,pointer])
   return (
     <div>
-      
+     <h2>Counter Value = {counter}</h2>
+      <h2>Pointer Value = {pointer}</h2>
+      <button onClick={()=>setCounter(counter+10)}>Counter</button>
+      <button onClick={()=>setPointer(pointer-10)}>Pointer</button>
     </div>
   )
 }
